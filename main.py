7dd898da99ce325b2b47fa44cff91acc324225c1
John Philip Carreon <jccarreon3@up.edu.ph>
2022-10-16 09:38:42 +0800

    feat: detect coins with erosion and dilation

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ def scaleImage(img, scale=0.75):
 def getImageMidpoint(img): return (img.shape[1] // 2, img.shape[0] // 2)
 
 
-img1 = cv.imread('input/fruit.png', cv.IMREAD_GRAYSCALE)
+img1 = cv.imread('input/coins1.jpg', cv.IMREAD_GRAYSCALE)
+img1 = scaleImage(img1, 0.5)
 
 _, img2 = cv.threshold(img1, 150, 255, cv.THRESH_BINARY)
-
 
 img2 = cv.erode(img2, (3, 3), iterations=3)
 img2 = cv.dilate(img2, (3, 3), iterations=3)
 
 img3 = cv.Canny(img2, 50, 150)
 
-cv.imshow("Img1", img1)
-cv.imshow("Img2", img2)
-cv.imshow("Img3", img3)
+cv.imshow("Original", img1)
+cv.imshow("Separation", img2)
+cv.imshow("Edge Detection", img3)
 
 cv.waitKey(0)
