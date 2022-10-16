9af28b873a7cb0b9e0e149c33729fe2f0c48d9db
John Philip Carreon <jccarreon3@up.edu.ph>
2022-10-16 09:54:59 +0800

    feat: save output to files

--- main.py
+++ main.py
@@ -14,18 +14,21 @@ def scaleImage(img, scale=0.75):
 def getImageMidpoint(img): return (img.shape[1] // 2, img.shape[0] // 2)
 
 
-img1 = cv.imread('input/coins1.jpg', cv.IMREAD_GRAYSCALE)
-img1 = scaleImage(img1, 0.5)
+img1 = cv.imread('./input/coins1.jpg', cv.IMREAD_GRAYSCALE)
 
-_, img2 = cv.threshold(img1, 150, 255, cv.THRESH_BINARY)
+_, img2 = cv.threshold(img1, 130, 255, cv.THRESH_BINARY)
 
-img2 = cv.erode(img2, (3, 3), iterations=3)
-img2 = cv.dilate(img2, (3, 3), iterations=3)
+cv.imwrite("./output/output1.jpg", img2)
 
-img3 = cv.Canny(img2, 50, 150)
 
-cv.imshow("Original", img1)
-cv.imshow("Separation", img2)
-cv.imshow("Edge Detection", img3)
+dilate = cv.erode(img2, (4, 4), iterations=3)
+cv.imwrite("./output/output_erode.jpg", dilate)
+
+erode = cv.dilate(img2, (4, 4), iterations=3)
+cv.imwrite("./output/output_dilate.jpg", erode)
+
+img3 = cv.Canny(erode, 50, 150)
+
+cv.imwrite("./output/output2.jpg", img3)
 
 cv.waitKey(0)
