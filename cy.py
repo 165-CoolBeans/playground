5293cb123a545bc296315da8e96d21719c440801
Cyrus Jude Cardano <cecardano@up.edu.ph>
2022-12-07 12:42:33 +0800

    feat: basic object detection part1 (canny bug)

--- cy.py
+++ cy.py
@@ -2,16 +2,41 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv.imread("./input/scoot.jpg")
+# util fxn; scale output to fit in screen
+def smolShow(img, caption):
+    scale_percent = 20                               # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
 
-# img.shape [height, width]
+    resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 
-width = int(img.shape[1] * 40/100)
-height = int(img.shape[0] * 40/100)
+    cv.imshow(caption, resized)
 
-img2 = cv.resize(img, (width, height), interpolation = cv.INTER_AREA)
+# read image
+img = cv.imread('./input/pollen1.jpg')
 
-cv.imshow(str(img2.shape), img2)
+# grayscale image
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# remove noise by using Gaussian blur
+img_blur = cv.GaussianBlur(img_gray, (51,51), 0)
+
+# use canny edge detection to get object edges; stuck here; outputs nothing on img_blur input
+img_canny = cv.Canny(img_blur, 0, 2000)
+
+# show images
+smolShow(img, "default")
+smolShow(img_gray, "grayscaled")
+smolShow(img_blur, "blurred")
+smolShow(img_canny, "canny")
+
 cv.waitKey(0)
+cv.destroyAllWindows()
 
-cv.imwrite("./output/_out.jpg", img2)
+# save image
+#cv.imwrite('./outbin/gray.jpg', img_gray)
+#cv.imwrite('./outbin/blur.jpg', img_blur)
+cv.imwrite('./outbin/canny.jpg', img_canny)
+
+print("Operation Done")
